5de1e1cc4a3ebe02fa2daa3b640648f4d60d112a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-04 21:43:13 +0300

    basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache inherits from baseCaching
+    """
+    def __init__(self):
+        """ Initialize BasicCache
+        """
+        super().__init__()
+    pass
