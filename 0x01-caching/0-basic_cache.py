ab3ff42af60a831540815c9ca5b77f8b3ec661a5
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-05-04 22:31:20 +0300

    LIFO

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,10 +7,34 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """
-    BasicCache inherits from baseCaching
+    BasicCache class that inherits from BaseCaching
     """
+
     def __init__(self):
-        """ Initialize BasicCache
+        """
+        Initialize BasicCache instance
         """
         super().__init__()
-    pass
+
+    def put(self, key, item):
+        """
+        Add key-value pair to the cache
+
+        Args:
+            key: Key to be added
+            item: Value to be associated with the key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve value associated with the given key from cache
+
+        Args:
+            key: Key to retrieve the value for
+
+        Returns:
+            Value associated with the key if found, None otherwise
+        """
+        return self.cache_data.get(key, None)
