aa79784bc2d568c7ced647cd6206a49ed99d7e91
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-05-04 23:16:22 +0300

    LRU

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,48 +1,68 @@
 #!/usr/bin/env python3
-"""Create a class LRUCache that inherits from
-BaseCaching and is a caching system:
-"""
+"""Create a class LRUCache that inherits from BaseCaching.
 
+This class implements a Least Recently Used (LRU) caching system.
+
+Attributes:
+    cache_data (dict): Dictionary to store key-value pairs for caching.
+    usedKeys (list): List to track the order of key usage (most recent keys are at the end).
+"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-
 class LRUCache(BaseCaching):
-    """_summary_
-    """
+    """LRUCache class inherits from BaseCaching."""
 
     def __init__(self):
-        """_summary_
+        """Initialize LRUCache instance.
+
+        Calls the __init__ method of the BaseCaching class and initializes usedKeys list.
         """
         super().__init__()
         self.usedKeys = []
 
     def put(self, key, item):
-        """_summary_
+        """Add a key-value pair to the cache.
+
+        If the cache is full (reached the MAX_ITEMS limit), remove the least recently used item
+        to make space for the new item (LRU algorithm).
 
         Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
+            key: Key of the item to be added.
+            item: Value of the item to be added.
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if key in self.usedKeys:
+            # Move the key to the end of the usedKeys list (recently used)
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+        else:
+            # Add the key to the end of the usedKeys list (newly added)
+            self.usedKeys.append(key)
+
+        if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+            # Cache is full, remove the least recently used item (LRU)
+            discard = self.usedKeys.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
+        """Retrieve the value associated with the given key from the cache.
+
+        If the key exists in the cache, move
+        it to the end of the usedKeys list (recently used).
 
         Args:
-                        key (_type_): _description_
+            key: Key of the item to retrieve.
+
+        Returns:
+            Value associated with the key if found in the cache, None otherwise.
         """
-        if key is not None and key in self.cache_data.keys():
+        if key is not None and key in self.cache_data:
+            # Move the key to the end of the usedKeys list (recently used)
             self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
+            return self.cache_data[key]
         return None
