e95d9d25dc0702a8277e4e2128ee79b059cdbb6b
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-05-05 11:53:23 +0300

    i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""A simple flask app
+ instantiate the Babel object in your app. Store it in a
+ module-level variable named babe
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """_summary_
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# configure the flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """_summary_
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
